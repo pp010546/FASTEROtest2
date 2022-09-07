00b35f9ed1e9c2bf67c522ba461658235fc5ef35
Philip <philip0105@gmail.com>
2022-09-07 11:49:11 +0800

    test - 3

--- FASTEROtest2t/src/main/java/Philip.java
+++ FASTEROtest2t/src/main/java/Philip.java
@@ -4,6 +4,7 @@ public class Philip {
 	public static void main(String[] args) {
 		System.out.println("let's go~~~");
 		System.out.println("let's go~~~");
+		System.out.println("let's go~~~");
 	}
 	
 }
