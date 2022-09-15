e215b6510b9808651279326801ec6c4780458fa6
Pei <m34672@gmail.com>
2022-09-15 16:17:59 +0800

    3333

--- FASTEROtest2t/src/main/java/fastero/model/vo/OrderMasterVO.java
+++ FASTEROtest2t/src/main/java/fastero/model/vo/OrderMasterVO.java
@@ -1,6 +1,4 @@
-package com.fastero.model.vo;
-
-import java.sql.Date;
+package fastero.model.vo;
 
 public class OrderMasterVO {
 //	order_id, user_id, store_id, order_status, order_amount, order_time, update_time, order_remark
@@ -12,55 +10,69 @@ public class OrderMasterVO {
 	private String orderTime;
 	private String updateTime;
 	private String orderRemark;
-	
+
 	public Integer getOrderId() {
 		return orderId;
 	}
+
 	public void setOrderId(Integer orderId) {
 		this.orderId = orderId;
 	}
+
 	public Integer getUseId() {
 		return useId;
 	}
+
 	public void setUseId(Integer useId) {
 		this.useId = useId;
 	}
+
 	public Integer getStoreId() {
 		return storeId;
 	}
+
 	public void setStoreId(Integer storeId) {
 		this.storeId = storeId;
 	}
+
 	public Integer getOrderStatus() {
 		return orderStatus;
 	}
+
 	public void setOrderStatus(Integer orderStatus) {
 		this.orderStatus = orderStatus;
 	}
+
 	public Integer getOrderAmount() {
 		return orderAmount;
 	}
+
 	public void setOrderAmount(Integer orderAmount) {
 		this.orderAmount = orderAmount;
 	}
+
 	public String getOrderTime() {
 		return orderTime;
 	}
+
 	public void setOrderTime(String orderTime) {
 		this.orderTime = orderTime;
 	}
+
 	public String getUpdateTime() {
 		return updateTime;
 	}
+
 	public void setUpdateTime(String updateTime) {
 		this.updateTime = updateTime;
 	}
+
 	public String getOrderRemark() {
 		return orderRemark;
 	}
+
 	public void setOrderRemark(String orderRemark) {
 		this.orderRemark = orderRemark;
 	}
 
-	
 }
---
+++ FASTEROtest2t/src/main/java/fastero/model/vo/testVO.java
@@ -0,0 +1,5 @@
+package fastero.model.vo;
+
+public class testVO {
+
+}
