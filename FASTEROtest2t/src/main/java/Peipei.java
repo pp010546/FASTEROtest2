fbaabc79140f5cddff450c0c41ea528d15ff8e3f
Pei <m34672@gmail.com>
2022-09-16 11:21:38 +0800

    cute

--- FASTEROtest2t/src/main/java/Peipei.java
+++ FASTEROtest2t/src/main/java/Peipei.java
@@ -4,6 +4,7 @@ public class Peipei {
 	public static void main(String[] args) {
 		System.out.println("I'm here!!!");
 		System.out.println("I'm here!!!");
+		System.out.println("I'm so cute!!!");
 
 	}
 
